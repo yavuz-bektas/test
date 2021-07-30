9c7d676a5ad94bd876476481488864f2ab3e2df2
Yavuz <bektasy8@gmail.com>
2021-07-30 23:07:11 +0300

    master

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ import os , random
 
 for i in range(200):
     d = str(i) + 'days ago'
-    rand = random.randrange(7, 12)
+    rand = random.randrange(10, 11)
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
